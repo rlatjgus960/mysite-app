a62b6e3d9e00e0ab6d6e069d1104949d114b564b
rlatjgus960 <rlatjgus960@naver.com>
2021-08-20 21:19:55 +0900

    8/20일 데이터통신

--- app/src/main/java/com/javaex/mysite/ListActivity.java
+++ app/src/main/java/com/javaex/mysite/ListActivity.java
@@ -2,15 +2,25 @@ package com.javaex.mysite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.javaex.vo.GuestbookVo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,53 +33,133 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        //데이터를 가져온다(서버로부터) -- 임시
-        List<GuestbookVo> guestbookList = getListFromServer();
-        Log.d("javaStudy", "서버로부터 데이터 수신");
-        Log.d("javaStudy", "List:"+guestbookList.toString());
-
         //ListView를 객체화 한다
-        lvGuestbookList = (ListView) findViewById(R.id.lvGuestbookList);
+        lvGuestbookList = (ListView)findViewById(R.id.lvGuestbookList);
 
-        //어댑터를 생성
-        GuestbookListAdapter guestbookListAdapter = new GuestbookListAdapter(getApplicationContext(), R.id.lvGuestbookList, guestbookList);
 
-        //리스트뷰에 어댑터를 세팅한다
-        lvGuestbookList.setAdapter(guestbookListAdapter);
+        //데이터를 가져온다(서버로부터) -- 가상
+        //List<GuestbookVo> guestbookList = getListFromServer();
 
-        //리스트의 클릭 이벤트
+        //나가서 일해라 --> 데이터 가져오기 --> 화면에 그리기(어댑터)
+        ListAsyncTask listAsyncTask = new ListAsyncTask();
+        listAsyncTask.execute();
+
+
+
         lvGuestbookList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //현재 클릭한 뷰의 리스트의 index값
+                //현재 클릭한 뷰의 리스트의 index 값
                 Log.d("javaStudy", "index: "+i);
 
                 //화면에 있는 값을 읽어온다
                 TextView txtContent = (TextView)view.findViewById(R.id.txtContent);
-                Log.d("javaStudy", "txtContent: "+txtContent.getText().toString());
+                Log.d("javaStudy", "Content: "+txtContent.getText().toString());
 
-                //화면에 출력되지 않은 데이터를 가져올때 ---> 리스트의 값을 사용할때
-                GuestbookVo guestbookVo = (GuestbookVo) adapterView.getItemAtPosition(i);
-
-                Log.d("javaStudy", "vo="+guestbookVo.toString());
-                Log.d("javaStudy", "vo.regDate="+guestbookVo.getRegDate());
+                //화면에 출력되지 않은 데이터를 가져올때 --> 리스트의 값을 사용할때
+                GuestbookVo guestbookVo = (GuestbookVo)adapterView.getItemAtPosition(i);
+                Log.d("javaStudy", "vo: "+guestbookVo.toString());
+                Log.d("javaStudy", "vo.regDate: "+guestbookVo.getRegDate());
 
                 //클릭한 아이템의 pk값을 읽어온다
                 int no = guestbookVo.getNo();
-                Log.d("javaStudy", "vo.no="+guestbookVo.getNo());
+                Log.d("javaStudy", "vo.no: "+no);
+
             }
         });
+    }
+
+    //이너클래스
+    public class ListAsyncTask extends AsyncTask<Void, Integer, List<GuestbookVo>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected List<GuestbookVo> doInBackground(Void... voids) {
+
+            //서버에 연결을 한다
+            List<GuestbookVo> guestbookList = null;
+            try {
+                URL url = new URL("http://192.168.0.9:8088/mysite5/api/guestbook/list"); //url 생성
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection(); //url 연결
+                conn.setConnectTimeout(10000); // 10초 동안 기다린 후 응답이 없으면 종료
+                conn.setRequestMethod("POST"); // 요청방식 POST
+                conn.setRequestProperty("Content-Type", "application/json"); //요청시 데이터 형식 json
+                conn.setRequestProperty("Accept", "application/json"); //응답시 데이터 형식 json
+                conn.setDoOutput(true); //OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
+                conn.setDoInput(true); //InputStream으로 서버로 부터 응답을 받겠다는 옵션.
+
+                int resCode = conn.getResponseCode(); // 응답코드 200이 정상
+                Log.d("javaStudy", ""+resCode);
+                if(resCode == 200){ //정상이면
+
+                    //Stream 을 통해 통신한다
+                    //데이타 형식은 json으로 한다.
+                    InputStream is = conn.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String jsonData = "";
+                    while(true) {
+                        String line = br.readLine();
+                        if(line == null) {
+                            break;
+                        }
+
+                        jsonData = jsonData + line;
+                    }
+
+                    Log.d("javaStudy", "jsonData --> "+jsonData);
+                    Gson gson = new Gson();
+                    guestbookList = gson.fromJson(jsonData, new TypeToken<List<GuestbookVo>>(){}.getType());
+
+                    Log.d("javaStudy", "doInBackground: " + guestbookList.size());
+                    Log.d("javaStudy", "doInBackground: " + guestbookList.get(0).getName());
 
 
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //요청을 한다
+            //응답을 받는다 json --> java 객체로 변환(List<GuestbookVo> guestbookList)
+
+
+            return guestbookList;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(List<GuestbookVo> guestbookList) {
+            Log.d("javaStudy", "onPostExecute: " + guestbookList.size());
+            Log.d("javaStudy", "onPostExecute: " + guestbookList.get(0).getName());
+
+            //Adapter 생성
+            GuestbookListAdapter guestbookListAdapter = new GuestbookListAdapter(getApplicationContext(), R.id.lvGuestbookList, guestbookList);
+
+            //ListView(Adapter)
+            lvGuestbookList.setAdapter(guestbookListAdapter);
+
+            super.onPostExecute(guestbookList);
+        }
     }
 
 
-
-    //방명록 정보 만들기(가상)
+    /*
+    //방명록 정보 만들기 (가상)
     public List<GuestbookVo> getListFromServer() {
         List<GuestbookVo> guestbookList = new ArrayList<GuestbookVo>();
 
-        for(int i=0; i<50; i++) {
+        for(int i=1; i<=50; i++) {
             GuestbookVo guestbookVo = new GuestbookVo();
             guestbookVo.setNo(i);
             guestbookVo.setName("정우성");
@@ -81,11 +171,6 @@ public class ListActivity extends AppCompatActivity {
 
         return guestbookList;
     }
-
-
-
-
-
-
+    */
 
 }
--- app/src/main/java/com/javaex/mysite/MainActivity.java
+++ app/src/main/java/com/javaex/mysite/MainActivity.java
@@ -1,10 +1,8 @@
 package com.javaex.mysite;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -12,71 +10,166 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.gson.Gson;
 import com.javaex.vo.GuestbookVo;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.Buffer;
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-    //필드
+    //field
     private Button btnWrite;
     private EditText edtName;
     private EditText edtPassword;
     private EditText edtContent;
 
 
-    //생성자
-    //gs
-    //일반
+    //constructor
+
+    //method g/s
+
+    //method general
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnWrite = (Button)findViewById(R.id.btnWrite);
-        edtName = (EditText)findViewById(R.id.edtName);
-        edtPassword = (EditText)findViewById(R.id.edtPassword);
-        edtContent = (EditText)findViewById(R.id.edtContent);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        edtContent = (EditText) findViewById(R.id.edtContent);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); //위젯 말고 androidx로 임포트
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setTitle("방명록 쓰기"); //xml에서 속성을 찾아서 처리
 
-        //저장버튼을 클릭할때
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("방명록쓰기");
+
+        //저장버튼 클릭할때
         btnWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("javaStudy", "저장버튼 클릭");
-                //방명록데이터를 Vo로 만든다
+                Log.d("javaStudy", "btnWrite onClick: ");
+
+                // vo로만들기
                 String name = edtName.getText().toString();
                 String password = edtPassword.getText().toString();
                 String content = edtContent.getText().toString();
 
                 GuestbookVo guestbookVo = new GuestbookVo(name, password, content);
-                Log.d("javaStudy", guestbookVo.toString());
+                Log.d("javaStudy", "vo = " + guestbookVo.toString());
 
-                //서버에 전송하고
+                //서버로 전송
                 Log.d("javaStudy", "서버 전송");
-                
+                WriteAsyncTask writeAsyncTask = new WriteAsyncTask();
+                writeAsyncTask.execute(guestbookVo);
+
+
                 //리스트 액티비티로 전환
-                Intent intent = new Intent(MainActivity.this, ListActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                //startActivity(intent);
+
             }
         });
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Log.d("javaStudy", "home버튼 클릭");
-        Log.d("javaStudy", "item.getItemId()-->"+item.getItemId());
-        Log.d("javaStudy", "android.R.id.home"+android.R.id.home);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.d("javaStudy", "onOptionsItemSelected: ");
 
-        switch(item.getItemId()){
+
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 return true;
-
             default:
                 return super.onOptionsItemSelected(item);
         }
+
+
     }
+
+    //innerclass
+    public class WriteAsyncTask extends AsyncTask<GuestbookVo, Intent, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(GuestbookVo... guestbookVos) {
+
+            Log.d("javaStudy", "doInBackground()");
+            Log.d("javaStudy", "vo="+guestbookVos[0].toString());
+
+            //vo-->json
+            Gson gson = new Gson();
+            String json = gson.toJson(guestbookVos[0]);
+            Log.d("javaStudy", "json-->"+json);
+
+            //데이터전송
+                //접속정보
+                //outputStream(json --> body)
+            try {
+                URL url = new URL("http://192.168.0.9:8088/mysite5/api/guestbook/write2"); //url 생성
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //url 연결
+                conn.setConnectTimeout(10000); // 10초 동안 기다린 후 응답이 없으면 종료
+                conn.setRequestMethod("POST"); // 요청방식 POST
+                conn.setRequestProperty("Content-Type", "application/json"); //요청시 데이터 형식 json
+                conn.setRequestProperty("Accept", "application/json"); //응답시 데이터 형식 json
+                conn.setDoOutput(true); //OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
+                conn.setDoInput(true); //InputStream으로 서버로 부터 응답을 받겠다는 옵션.
+
+                //outputStream(json --> body)
+                OutputStream os = conn.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+
+                bw.write(json);
+                bw.flush();
+
+                int resCode = conn.getResponseCode(); // 응답코드 200이 정상
+                Log.d("javaStudy", "resCode-->"+resCode);
+
+                if(resCode == HttpURLConnection.HTTP_OK){
+                    //리스트 액티비티로 전환
+                    Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                    startActivity(intent);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Intent... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
+
 }
